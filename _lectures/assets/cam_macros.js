3064c631cdc93ab008d135f3fae4adb1822ad0db
Charles Lanfear <cclanfear@gmail.com>
2025-10-10 15:50:03 +0100

    Initial commit

---
+++ _lectures/assets/cam_macros.js
@@ -0,0 +1,8 @@
+remark.macros.width = function(w) {
+  var url = this;
+  return '<img src="' + url + '" style="width: ' + w + '" />';
+};
+remark.macros.height = function(h) {
+  var url = this;
+  return '<img src="' + url + '" style="height: ' + h + '" />';
+};
